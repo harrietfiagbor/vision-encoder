ea03e772fc2a024e2bffb8c8a6ae732fe661ffb8
harrietfiagbor <harrietfiagbor@gmail.com>
2023-09-20 13:48:38 +0000

    initial commit of vision encoder files

---
+++ src/clip-encoder.py
@@ -0,0 +1,39 @@
+import torch
+from PIL import Image
+import argparse
+
+from transformers import CLIPProcessor, CLIPModel
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--data_url", default=None)
+parser.add_argument("--vision_tower", default="openai/clip-vit-base-patch14")
+args = parser.parse_args()
+
+
+class CLIPVisionTower():
+    def __init__(self, args):
+
+        self.is_loaded = False
+        self.vision_tower_name = args.vision_tower
+
+    def load_model(self):
+
+        self.processor = CLIPProcessor.from_pretrained(self.vision_tower_name)
+        self.vision_tower = CLIPModel.from_pretrained(self.vision_tower_name)
+
+    @torch.inference_mode
+    def forward(self, image, text):
+        with open(args.data_url) as data:
+            for _values in data.values():
+                image = _values['image']
+                text = _values['text']
+                inputs = self.processor(
+                    text=text,
+                    images=image,
+                    return_tensors="pt",
+                    padding=True
+                )
+                outputs = self.vision_tower(**inputs)
+                logits_per_image = outputs.logits_per_image
+                probs = logits_per_image.softmax(dim=1)
